bb6a92c8b8d7fb722a8ae01f8b11831edff3e085
Ewen Maclean <ewenmaclean@gmail.com>
2018-07-31 14:28:23 +0100

    simplest possible dependency version

--- src/client/request/index.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from "vscode";
-import * as client from "vscode-languageclient";
-
-export function registerAll(_context: vscode.ExtensionContext, _languageClient: client.LanguageClient): void {
-  return;
-}
