d989be95ed4cfdb1eace6221a67335195ac07817
Simon Cruanes <simon.cruanes.2007@m4x.org>
2019-04-23 16:49:24 -0500

    feat: progress on the diagnostic updates

--- src/checker/index.ts
+++ src/checker/index.ts
@@ -1,11 +1,14 @@
+"use strict";
+
 import * as vscode from "vscode";
 import * as proc from "child_process";
-import { promisify } from "util";
+import * as net from "net";
+import { promisify, inspect } from "util";
 import * as assert from "assert";
-import { DiagnosticSeverity } from "vscode-languageclient";
 
 enum state {
   Start,
+  Connecting,
   Connected,
   Disposed,
 }
@@ -19,19 +22,35 @@ export interface IRange {
   end: IPosition;
 }
 
-const PosToIPos = (p: vscode.Position): IPosition => {
+function PosToIPos(p: vscode.Position): IPosition {
   return { line: p.line, char: p.character };
-};
-export const RangeToIRange = (r: vscode.Range): IRange => {
+}
+export function RangeToIRange(r: vscode.Range): IRange {
   return { start: PosToIPos(r.start), end: PosToIPos(r.end) };
-};
+}
 
-const IPosToPos = (p: IPosition): vscode.Position => {
+function IPosToPos(p: IPosition): vscode.Position {
   return new vscode.Position(p.line, p.char);
-};
-const IRangeToRange = (r: IRange): vscode.Range => {
+}
+function IRangeToRange(r: IRange): vscode.Range {
   return new vscode.Range(IPosToPos(r.start), IPosToPos(r.end));
-};
+}
+
+function waitForConnectionPromise(server: net.Server): Promise<net.Socket> {
+  return new Promise((resolve, _) => {
+    server.once("connection", (sock: net.Socket) => {
+      resolve(sock);
+    });
+  });
+}
+
+function listenPromise(server: net.Server): Promise<void> {
+  return new Promise((resolve, _) => server.listen(() => resolve()));
+}
+
+function isDocIml(d: vscode.TextDocument): boolean {
+  return d.fileName.includes(".iml"); // TODO: improve on that!
+}
 
 // messages to imandra
 namespace msg {
@@ -102,57 +121,52 @@ namespace response {
  * A document with its current list of diagnostics.
  */
 class DocState implements vscode.Disposable {
-  private _diagnostics: vscode.Diagnostic[] = [];
-  private _doc: vscode.TextDocument;
-  private _diagsColl: vscode.DiagnosticCollection;
+  private curDiags: vscode.Diagnostic[] = [];
+  private doc: vscode.TextDocument;
+  private diagsColl: vscode.DiagnosticCollection;
   constructor(d: vscode.TextDocument, diags: vscode.DiagnosticCollection) {
-    this._doc = d;
-    this._diagsColl = diags;
+    this.doc = d;
+    this.diagsColl = diags;
   }
   public uri(): vscode.Uri {
-    return this._doc.uri;
+    return this.doc.uri;
   }
   public uriStr(): string {
-    return this._doc.uri.toString();
+    return this.doc.uri.fsPath;
   }
   public version(): number {
-    return this._doc.version;
+    return this.doc.version;
   }
   public getText(): string {
-    return this._doc.getText();
-  }
-  public diagnostics(): vscode.Diagnostic[] {
-    return this._diagnostics;
+    return this.doc.getText();
   }
   public addDiagnostic(version: number, d: vscode.Diagnostic) {
     if (version === this.version()) {
-      this._diagnostics.push(d);
+      this.curDiags.push(d);
       // update the diagnostic collection
-      this._diagsColl.delete(this._doc.uri);
-      this._diagsColl.set(this.uri(), this._diagnostics);
+      this.diagsColl.set(this.uri(), this.curDiags);
     }
   }
   public updateDoc(d: vscode.TextDocument) {
-    const curVer = this.version();
-    assert(d.uri.toString() === this.uriStr() && d.version >= curVer); // same doc
-    this._doc = d;
-    if (d.version > curVer) {
-      // clear diagnostics
-      this._diagnostics.length = 0;
-      this._diagsColl.delete(this.uri());
-    }
+    assert(d.uri.fsPath.toString() === this.uriStr() && d.version >= this.version()); // same doc
+    this.doc = d;
+    // clear diagnostics
+    this.curDiags.length = 0;
+    this.diagsColl.delete(this.uri());
   }
   public dispose() {
-    this._diagsColl.delete(this.uri()); // clear diagnostics for this document
+    this.diagsColl.delete(this.uri()); // clear diagnostics for this document
   }
 }
 
-const PING_FREQ = 5 * 1000; // in seconds
-const MAX_EPOCH_MISSED = 6; // maximum number of missed "ping" we accept before concluding the server is dead
+const PING_FREQ = 20 * 1000; // in ms
+const MAX_EPOCH_MISSED = 3; // maximum number of missed "ping" we accept before concluding the server is dead
 
 export class ImandraServer implements vscode.Disposable {
   private st: state = state.Start;
-  private subproc: proc.ChildProcess; // connection to imandra server
+  private subprocConn: undefined | net.Socket;
+  private subproc: undefined | proc.ChildProcess; // connection to imandra server
+  private server: net.Server; // listen for a connection from imandra
   private docs: Map<string, DocState> = new Map(); // set of active docs, by their uri string
   private subscriptions: vscode.Disposable[] = [];
   private diagnostics: vscode.DiagnosticCollection = vscode.languages.createDiagnosticCollection("imandra");
@@ -160,7 +174,15 @@ export class ImandraServer implements vscode.Disposable {
   private lastPongEpoch: number = 0;
 
   // setup connection to imandra-vscode-server
-  private setupConn() {
+  private setupConn(subproc: proc.ChildProcess, sock: net.Socket) {
+    this.subproc = subproc;
+    this.subprocConn = sock;
+    if (this.subproc.pid) {
+      this.st = state.Connected;
+    } else {
+      this.st = state.Disposed;
+      return;
+    }
     this.subproc.on("close", (code, signal) => {
       console.log(`imandra-vscode closed with code=${code}, signal=${signal}`);
       this.st = state.Disposed;
@@ -168,18 +190,19 @@ export class ImandraServer implements vscode.Disposable {
     this.subproc.on("exit", code => {
       console.log(`imandra-vscode exited with ${code}`);
       this.st = state.Disposed;
-      this.subproc.kill();
+      subproc.kill();
     });
-    this.subproc.stdout.on("data", j => {
+    sock.on("data", j => {
       console.log(`got message from imandra: ${j}`);
       const res = JSON.parse(j.toString()) as response.Res;
       this.handleRes(res);
     });
     // regularly ping the server
     const timer = setInterval(() => {
+      if (!this.connected()) return;
       const missed = this.pingEpoch - this.lastPongEpoch;
       if (missed > MAX_EPOCH_MISSED) {
-        console.log(`missed ${missed} "ping" epochs, consider the server is dead`);
+        console.log(`missed ${missed} "ping" epochs, consider the server as dead`);
         this.dispose();
       }
       this.sendMsg({ kind: "ping", epoch: ++this.pingEpoch });
@@ -193,23 +216,14 @@ export class ImandraServer implements vscode.Disposable {
   }
 
   public constructor() {
-    console.log("connecting to imandra server...");
-    const opts = ["-d", "4"];
-    // TODO: use `opam exec imandra-vscode-server` instead
-    this.subproc = proc.spawn("imandra-vscode-server", opts);
-    if (this.subproc.pid) {
-      this.setupConn();
-      this.st = state.Connected;
-    } else {
-      this.st = state.Disposed;
-    }
-    console.log(`state: ${state[this.st]}, PID ${this.subproc ? this.subproc.pid : 0}`);
+    this.server = net.createServer();
   }
 
   public dispose() {
     this.subscriptions.forEach(x => x.dispose());
     this.subscriptions.length = 0;
-    if (this.st === state.Connected) {
+    this.server.close();
+    if (this.connected() && this.subproc) {
       this.st = state.Disposed;
       this.subproc.kill();
     }
@@ -221,21 +235,23 @@ export class ImandraServer implements vscode.Disposable {
 
   /// Send a message to the underlying server.
   public async sendMsg(m: msg.Msg): Promise<void> {
-    if (!this.connected()) {
+    const conn = this.subprocConn;
+    if (!this.connected() || conn === undefined) {
       console.log("do not send message, imandra-vscode disconnected");
       throw new Error("imandra-vscode disconnected");
     }
     const j = JSON.stringify(m);
     //console.log(`send msg ${j}`);
-    const isDone = this.subproc.stdin.write(j);
+    const isDone = conn.write(j);
     if (!isDone) {
-      await promisify(f => this.subproc.stdin.once("drain", () => f(null, {})));
+      await promisify(f => conn.once("drain", () => f(null, {})));
     }
     return;
   }
 
   private async addDoc(d: vscode.TextDocument) {
-    const key = d.uri.toString();
+    if (!isDocIml(d)) return;
+    const key = d.uri.fsPath.toString();
     const isNew = !this.docs.has(key);
     // insert a new docstate
     this.docs.set(key, new DocState(d, this.diagnostics));
@@ -249,7 +265,8 @@ export class ImandraServer implements vscode.Disposable {
   }
 
   private async removeDoc(d: vscode.TextDocument) {
-    const key = d.uri.toString();
+    if (!isDocIml(d)) return;
+    const key = d.uri.fsPath.toString();
     const dSt = this.docs.get(key);
     if (dSt) {
       this.docs.delete(key);
@@ -259,8 +276,9 @@ export class ImandraServer implements vscode.Disposable {
   }
 
   private async changeDoc(d: vscode.TextDocumentChangeEvent) {
-    console.log(`[connected: ${this.connected()}]: change doc ${d}`);
-    const key = d.document.uri.toString();
+    if (!isDocIml(d.document)) return;
+    console.log(`[connected: ${this.connected()}]: change doc ${d.document.uri} version ${d.document.version}`);
+    const key = d.document.uri.fsPath.toString();
     {
       // update stored document
       const dState = this.docs.get(key);
@@ -268,7 +286,7 @@ export class ImandraServer implements vscode.Disposable {
     }
     await this.sendMsg({
       kind: "doc_update",
-      uri: d.document.uri.toString(),
+      uri: d.document.uri.fsPath.toString(),
       new_content: d.document.getText(),
       version: d.document.version,
     });
@@ -278,23 +296,25 @@ export class ImandraServer implements vscode.Disposable {
   private handleRes(res: response.Res) {
     switch (res.kind) {
       case "valid": {
-        console.log("res: valid!");
+        console.log(`res: valid! (range ${inspect(res.range)})`);
         const d = this.docs.get(res.uri);
         if (d) {
           const r = IRangeToRange(res.range);
-          const sev = DiagnosticSeverity.Information;
+          const sev = vscode.DiagnosticSeverity.Information;
           const diag = new vscode.Diagnostic(r, res.msg, sev);
+          diag.source = "imandra";
           d.addDiagnostic(res.version, diag);
         }
         return;
       }
       case "error": {
-        console.log("res: error");
+        console.log(`res: error (range ${inspect(res.range)})`);
         const d = this.docs.get(res.uri);
         if (d) {
           const r = IRangeToRange(res.range);
-          const sev = DiagnosticSeverity.Error;
+          const sev = vscode.DiagnosticSeverity.Error;
           const diag = new vscode.Diagnostic(r, res.msg, sev);
+          diag.source = "imandra";
           d.addDiagnostic(res.version, diag);
         }
         return;
@@ -323,15 +343,38 @@ export class ImandraServer implements vscode.Disposable {
   }
 
   /// attach to changes in documents
-  public init() {
-    vscode.workspace.onDidOpenTextDocument(this.addDoc, this, this.subscriptions);
-    vscode.workspace.onDidCloseTextDocument(this.removeDoc, this, this.subscriptions);
-    vscode.workspace.onDidChangeTextDocument(this.changeDoc, this, this.subscriptions);
+  public async init() {
+    // listen on random port
+    console.log("connecting to imandra-vscode-server...");
+    // TODO: use `opam exec imandra-vscode-server` instead
+    await listenPromise(this.server);
+    const port = this.server.address().port;
+    const args = ["-d", "4", "--host", "127.0.0.1", "--port", port.toString()];
+    //console.log("call imandra-vscode-server with args ", args);
+    const sockP = waitForConnectionPromise(this.server);
+    const subproc = proc.spawn("imandra-vscode-server", args, { stdio: "inherit" });
+    if (!subproc.pid) {
+      this.dispose();
+      return;
+    }
+    console.log(`waiting for connection (pid: ${subproc.pid})...`);
+    const sock = await sockP;
+    console.log("got connection!");
+    if (this.subprocConn === undefined) {
+      this.setupConn(subproc, sock);
+    }
+    console.log(`state: ${state[this.st]}, PID ${this.subproc ? this.subproc.pid : 0}`);
+    if (this.connected()) {
+      for (const d of vscode.workspace.textDocuments) this.addDoc(d); // add existing docs
+      vscode.workspace.onDidOpenTextDocument(this.addDoc, this, this.subscriptions);
+      vscode.workspace.onDidCloseTextDocument(this.removeDoc, this, this.subscriptions);
+      vscode.workspace.onDidChangeTextDocument(this.changeDoc, this, this.subscriptions);
+    }
   }
 }
 
 export async function launch(_ctx: vscode.ExtensionContext): Promise<vscode.Disposable> {
   const server = new ImandraServer();
-  server.init();
+  await server.init();
   return Promise.resolve(server);
 }
